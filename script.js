3d00dce9af0f7922ec48d28512193f0febf43078
perna saidev <saidev.marolix@gmail.com>
2024-07-21 18:37:34 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var menuBtn = document.getElementById("menu_btn");
+        var navLinks = document.getElementById("nav_links");
+ 
+        menuBtn.addEventListener("click", () => {
+            navLinks.classList.toggle("open");
+        });
+
+      
